24ff6032759d2315ac2d17bf6cd5d805408c3a54
Aishwary Gupta <guptashark@protonmail.com>
2023-04-20 22:38:05 -0400

    Test case showing faulty print fn.

--- tests/jumbo_num_tests.c
+++ tests/jumbo_num_tests.c
@@ -2,26 +2,42 @@
 
 #include <assert.h>
 
+static void test_add() {
+
+  struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
+  struct pfx_02_jumbo_num *jn_02 = pfx_02_jumbo_num_new();
+  struct pfx_02_jumbo_num *jn_03 = pfx_02_jumbo_num_new();
+
+  pfx_02_jumbo_num_init(jn_01, 123);
+  pfx_02_jumbo_num_init(jn_02, 456);
+  pfx_02_jumbo_num_init(jn_03, 579);
+
+  pfx_02_jumbo_num_print(jn_01);
+  pfx_02_jumbo_num_print(jn_02);
+  pfx_02_jumbo_num_print(jn_03);
+
+  pfx_02_jumbo_num_add(jn_01, jn_02);
+  pfx_02_jumbo_num_eq(jn_01, jn_03);
+
+  pfx_02_jumbo_num_print(jn_01);
+  pfx_02_jumbo_num_print(jn_02);
+  pfx_02_jumbo_num_print(jn_03);
+}
 
 int main(void) {
 
   struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_02 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_03 = pfx_02_jumbo_num_new();
-  struct pfx_02_jumbo_num *jn_04 = pfx_02_jumbo_num_new();
 
   pfx_02_jumbo_num_init(jn_01, 5);
   pfx_02_jumbo_num_init(jn_02, 5);
   pfx_02_jumbo_num_init(jn_03, 6);
-  pfx_02_jumbo_num_init(jn_04, 123456);
 
   assert(pfx_02_jumbo_num_eq(jn_01, jn_02));
   assert(pfx_02_jumbo_num_neq(jn_01, jn_03));
 
-  pfx_02_jumbo_num_print(jn_01);
-  pfx_02_jumbo_num_print(jn_02);
-  pfx_02_jumbo_num_print(jn_03);
-  pfx_02_jumbo_num_print(jn_04);
+  test_add();
 
   return 0;
 }
