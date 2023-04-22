82e050d3e141c3885851b6cd8c1a3d3f6d01fad1
Aishwary Gupta <guptashark@protonmail.com>
2023-04-21 22:39:41 -0400

    Test: jumbo num: add with different len nums

--- tests/jumbo_num_tests.c
+++ tests/jumbo_num_tests.c
@@ -2,6 +2,7 @@
 
 #include <assert.h>
 
+// Test with no carry - (same number of digits).
 static void test_add_01() {
 
   struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
@@ -12,18 +13,15 @@ static void test_add_01() {
   pfx_02_jumbo_num_init(jn_02, 456);
   pfx_02_jumbo_num_init(jn_03, 579);
 
-  pfx_02_jumbo_num_print(jn_01);
-  pfx_02_jumbo_num_print(jn_02);
-  pfx_02_jumbo_num_print(jn_03);
-
   pfx_02_jumbo_num_add(jn_01, jn_02);
   pfx_02_jumbo_num_eq(jn_01, jn_03);
 
-  pfx_02_jumbo_num_print(jn_01);
-  pfx_02_jumbo_num_print(jn_02);
-  pfx_02_jumbo_num_print(jn_03);
+  pfx_02_jumbo_num_free(jn_01);
+  pfx_02_jumbo_num_free(jn_02);
+  pfx_02_jumbo_num_free(jn_03);
 }
 
+// Test with carry - (same number of digits).
 static void test_add_02() {
 
   struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
@@ -36,6 +34,29 @@ static void test_add_02() {
 
   pfx_02_jumbo_num_add(jn_01, jn_02);
   assert(pfx_02_jumbo_num_eq(jn_01, jn_03));
+
+  pfx_02_jumbo_num_free(jn_01);
+  pfx_02_jumbo_num_free(jn_02);
+  pfx_02_jumbo_num_free(jn_03);
+}
+
+// Test with carry - (different number of digits).
+static void test_add_03() {
+
+  struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
+  struct pfx_02_jumbo_num *jn_02 = pfx_02_jumbo_num_new();
+  struct pfx_02_jumbo_num *jn_03 = pfx_02_jumbo_num_new();
+
+  pfx_02_jumbo_num_init(jn_01, 1);
+  pfx_02_jumbo_num_init(jn_02, 899);
+  pfx_02_jumbo_num_init(jn_03, 900);
+
+  pfx_02_jumbo_num_add(jn_01, jn_02);
+  assert(pfx_02_jumbo_num_eq(jn_01, jn_03));
+
+  pfx_02_jumbo_num_free(jn_01);
+  pfx_02_jumbo_num_free(jn_02);
+  pfx_02_jumbo_num_free(jn_03);
 }
 
 int main(void) {
@@ -53,6 +74,7 @@ int main(void) {
 
   test_add_01();
   test_add_02();
+  test_add_03();
 
   return 0;
 }
