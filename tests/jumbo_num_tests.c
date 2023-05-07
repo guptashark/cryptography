9d4cdfe3145c431570391ce64c7b9d5beb926de5
Aishwary Gupta <guptashark@protonmail.com>
2023-05-07 00:30:27 -0400

    Feat: simplified jumbo_num test fns.

--- tests/jumbo_num_tests.c
+++ tests/jumbo_num_tests.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <stdio.h>
 
-static void test_add(int a, int b, int expected) {
+static void test_add(int a, int b) {
   struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_02 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_03 = pfx_02_jumbo_num_new();
@@ -12,7 +12,7 @@ static void test_add(int a, int b, int expected) {
   pfx_02_jumbo_num_init(jn_01, 0);
   pfx_02_jumbo_num_init(jn_02, a);
   pfx_02_jumbo_num_init(jn_03, b);
-  pfx_02_jumbo_num_init(jn_04, expected);
+  pfx_02_jumbo_num_init(jn_04, a + b);
 
   pfx_02_jumbo_num_add(jn_01, jn_02, jn_03);
   assert(pfx_02_jumbo_num_eq(jn_01, jn_04));
@@ -43,7 +43,7 @@ static void test_add_str(char *a, char *b, char *expected) {
   pfx_02_jumbo_num_free(jn_04);
 }
 
-static void test_mult(int a, int b, int expected) {
+static void test_mult(int a, int b) {
   struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_02 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_03 = pfx_02_jumbo_num_new();
@@ -52,7 +52,7 @@ static void test_mult(int a, int b, int expected) {
   pfx_02_jumbo_num_init(jn_01, 0);
   pfx_02_jumbo_num_init(jn_02, a);
   pfx_02_jumbo_num_init(jn_03, b);
-  pfx_02_jumbo_num_init(jn_04, expected);
+  pfx_02_jumbo_num_init(jn_04, a * b);
 
   pfx_02_jumbo_num_mult(jn_01, jn_02, jn_03);
   assert(pfx_02_jumbo_num_eq(jn_01, jn_04));
@@ -63,7 +63,7 @@ static void test_mult(int a, int b, int expected) {
   pfx_02_jumbo_num_free(jn_04);
 }
 
-static void test_subt(int a, int b, int expected) {
+static void test_subt(int a, int b) {
   struct pfx_02_jumbo_num *jn_01 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_02 = pfx_02_jumbo_num_new();
   struct pfx_02_jumbo_num *jn_03 = pfx_02_jumbo_num_new();
@@ -72,7 +72,7 @@ static void test_subt(int a, int b, int expected) {
   pfx_02_jumbo_num_init(jn_01, 0);
   pfx_02_jumbo_num_init(jn_02, a);
   pfx_02_jumbo_num_init(jn_03, b);
-  pfx_02_jumbo_num_init(jn_04, expected);
+  pfx_02_jumbo_num_init(jn_04, a - b);
 
   pfx_02_jumbo_num_subt(jn_01, jn_02, jn_03);
   assert(pfx_02_jumbo_num_eq(jn_01, jn_04));
@@ -139,11 +139,6 @@ int main(void) {
 
   test_copy();
 
-  test_add(123, 456, 579);
-  test_add(250, 251, 501);
-  test_add(1, 100, 101);
-  test_add(1, 899, 900);
-
   test_add_str("123", "456", "579");
   test_add_str("250", "251", "501");
   test_add_str("1", "100", "101");
@@ -165,12 +160,13 @@ int main(void) {
 
   for (int i = 0; i < 300; ++i) {
     for (int j = 0; j < 300; ++j) {
-      test_mult(i, j, i * j);
+      test_add(i, j);
+      test_mult(i, j);
     }
   }
 
-  test_subt(333, 222, 111);
-  test_subt(222, 333, -111);
+  test_subt(333, 222);
+  test_subt(222, 333);
 
   test_print(5);
   test_print(-5);
